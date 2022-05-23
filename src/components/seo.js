a36de70648bbf47d6e49c83c7e4e380b1d570e5e
Serhii Mozhaiskyi <sergei.mozhaisky@gmail.com>
2022-05-23 20:16:18 +0300

    add 'title' tag

--- src/components/seo.js
+++ src/components/seo.js
@@ -16,6 +16,7 @@ const Seo = ({
   return (
     <Helmet title={seoTitle}>
       <html lang={config.siteLanguage} />
+      <title>{seoTitle}</title>
       <link rel="alternate" type="application/rss+xml" href={config.siteRss} title={config.siteTitle} />
       <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
