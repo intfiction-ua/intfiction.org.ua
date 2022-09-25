be9fe8606fa790caadb66c82cabbf8789a5bf602
Serhii Mozhaiskyi <sergei.mozhaisky@gmail.com>
2022-09-26 00:39:06 +0300

    fix code block; disable css purging

--- gatsby-config.js
+++ gatsby-config.js
@@ -157,9 +157,11 @@ const gatsbyPurgeCSS = {
     // printRejected: true, // Print removed selectors and processed file names
     develop: true, // Enable while using `gatsby develop`
     // tailwind: true, // Enable tailwindcss support
-    // whitelist: ['whitelist'], // Don't remove this selector
     // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
     // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
+    purgeCSSOptions: {
+      safelist: [ '.content pre', 'code' ], // Don't remove this selector
+    },
   },
 };
 
@@ -179,7 +181,7 @@ module.exports = {
     gatsbyPluginSharp,
     gatsbyTransformerRemark,
     gatsbyGoogleAnalytics,
-    gatsbyPurgeCSS,
+    // gatsbyPurgeCSS,
     'gatsby-plugin-sitemap',
   ],
 };
