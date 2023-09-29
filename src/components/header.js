1a7342b6607920900ea714b7346a44ce53b41b91
Serhii Mozhaiskyi <sergei.mozhaisky@gmail.com>
2023-09-29 22:57:08 +0300

    fix banner layout

--- src/components/header.js
+++ src/components/header.js
@@ -2,10 +2,8 @@ import React from 'react';
 import { Link } from 'gatsby';
 import config from '../config';
 
-// temporary remove 'block' className from 'nav' while banner is available
-
 const Header = () => (
-  <nav className="navbar is-primary">
+  <nav className="navbar is-primary block">
     <div className="navbar-brand">
       <div className="navbar-item">
         <img id="intfiction-logo" src="/logo/ifiction-ua-128.png" alt="ІЛ" />
