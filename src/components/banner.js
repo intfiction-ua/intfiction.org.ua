f420a620889f04bac2753de2e630ba3c9439c8bb
Serhii Mozhaiskyi <sergei.mozhaisky@gmail.com>
2024-06-27 22:43:22 +0300

    add festival 2024

--- src/components/banner.js
+++ src/components/banner.js
@@ -4,8 +4,8 @@ const Banner = () => (
   <div className="columns">
     <div className="column is-12">
       <div className="content banner">
-        <a href="https://itch.io/jam/ukrainian-interactive-fiction-festival-2023" style={{ color: '#000000', fontSize: '1.5rem', fontWeight: 'bold' }}>
-          Фестиваль української інтерактивної літератури 2023
+        <a href="https://itch.io/jam/ukrainian-interactive-fiction-festival-2024" style={{ color: '#000000', fontSize: '1.5rem', fontWeight: 'bold' }}>
+          Фестиваль української інтерактивної літератури 2024
         </a>
       </div>
     </div>
