b6d7e0c996419ac73c0db3631160c789efe06790
Serhii Mozhaiskyi <sergei.mozhaisky@gmail.com>
2025-10-13 12:55:18 +0300

    group GFP by year

--- src/components/garden-dates.js
+++ src/components/garden-dates.js
@@ -2,6 +2,18 @@ import React from 'react';
 import { useStaticQuery, graphql, Link } from 'gatsby';
 import { formatDateM } from '../utils';
 
+const GardenYear = ({ dates, activeClassName }) => dates.map((item) => (
+  <li key={item.fieldValue}>
+    <Link
+      to={`/gardenchapter/${item.fieldValue}/`}
+      key={item.fieldValue}
+      activeClassName={activeClassName}
+    >
+      {formatDateM(item.fieldValue)} <span className="tag">{item.totalCount}</span>
+    </Link>
+  </li>
+));
+
 const GardenDatesList = ({ activeClassName }) => {
   const data = useStaticQuery(graphql`
     query {
@@ -14,6 +26,15 @@ const GardenDatesList = ({ activeClassName }) => {
     }
   `);
   const dates = data.allMarkdownRemark.group.sort((a, b) => (a.fieldValue < b.fieldValue ? 1 : -1));
+  const years = dates.reduce((accumulator, item) => {
+    const key = item.fieldValue.substring(0, 4);
+    if (!accumulator[key]) {
+      accumulator[key] = [];
+    }
+    accumulator[key].push(item);
+    return accumulator;
+  }, {});
+  const yearList = Object.keys(years).sort((a, b) => (a.fieldValue < b.fieldValue ? 1 : -1));
 
   return (
     <ul className="menu-list is-size-6">
@@ -26,16 +47,13 @@ const GardenDatesList = ({ activeClassName }) => {
         </Link>
       </li>
       {
-        dates.map((item) => (
-          <li key={item.fieldValue}>
-            <Link
-              to={`/gardenchapter/${item.fieldValue}/`}
-              key={item.fieldValue}
-              activeClassName={activeClassName}
-            >
-              {formatDateM(item.fieldValue)} <span className="tag">{item.totalCount}</span>
-            </Link>
-          </li>
+        yearList.map((year) => (
+          <>
+            <li key={year} style={{ padding: '0.75em', backgroundColor: '#EEEEEE' }}>
+              <b>{year}</b>
+            </li>
+            <GardenYear dates={years[year]} activeClassName={activeClassName} />
+          </>
         ))
       }
     </ul>
